1914e597b817d4d6ecd624b28bad7522e43684d0
eashhyeon3232 <jihyeon-lou3232@naver.com>
2026-06-23 23:12:20 +0900

    feat : 검색 엔티티 추가

---
+++ src/main/java/com/example/gifbottary/domain/search/entity/SearchKeyword.java
@@ -0,0 +1,47 @@
+package com.example.gifbottary.domain.search.entity;
+
+import com.example.gifbottary.common.entity.BaseEntity;
+import com.example.gifbottary.domain.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@Table(name = "search_keyword")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SearchKeyword extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JoinColumn(name = "user_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    private String keyword;
+
+    private Integer searchCount;
+
+    private LocalDateTime lastSearchedAt;
+
+    public SearchKeyword(User user, String keyword) {
+        this.user = user;
+        this.keyword = keyword;
+        this.searchCount = 1;
+        this.lastSearchedAt = LocalDateTime.now();
+    }
+
+    public void increaseCount() {
+        this.searchCount++;
+        this.lastSearchedAt = LocalDateTime.now();
+    }
+
+    public void refreshLastSearchedAt() {
+        this.lastSearchedAt = LocalDateTime.now();
+    }
+}
