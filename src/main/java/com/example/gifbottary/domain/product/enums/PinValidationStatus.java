07ebc51f30f20874af1df103ffc521a6bb960bc5
eashhyeon3232 <jihyeon-lou3232@naver.com>
2026-06-24 10:34:35 +0900

    refactor : 메서드명 수정

--- src/main/java/com/example/gifbottary/domain/product/enums/PinValidationStatus.java
+++ src/main/java/com/example/gifbottary/domain/product/enums/PinValidationStatus.java
@@ -1,6 +1,6 @@
 package com.example.gifbottary.domain.product.enums;
 
-public enum PinValidateionStatus {
+public enum PinValidationStatus {
     PENDING,  // 검수 대기
     VALID,    // 검수 성공
     INVALID   // 검수 실패
