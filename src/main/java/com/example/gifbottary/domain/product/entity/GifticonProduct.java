b8dc488d351479088e5be45c8e6f2efbc8d53968
easyhyeon3232 <146900921+easyhyeon3232@users.noreply.github.com>
2026-06-24 11:12:57 +0900

    refactor : 유니크 제약 및 컬럼명 추가

--- src/main/java/com/example/gifbottary/domain/product/entity/GifticonProduct.java
+++ src/main/java/com/example/gifbottary/domain/product/entity/GifticonProduct.java
@@ -8,15 +8,23 @@ import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
-@Table(name = "gifticon_product")
+@Table(name = "gifticon_product", uniqueConstraints = {
+        @UniqueConstraint(name = "uk_gifticon_product_brand_name", columnNames = {"brand", "product_name"})
+    })
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class GifticonProduct  extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @C0lumn(nullable = false)
     private String brand;
+    
+    @Column(name = "product_name", nullable = false)
     private String productName;
+
+    @Column(nullable = false)
     private Integer faceValue;
     private String imageUrl;
 
