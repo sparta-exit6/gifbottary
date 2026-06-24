e75f9b4ffd40860e7bed1fb0e3f01a8385bcd586
Minjae <cmj011112@gmail.com>
2026-06-24 15:49:50 +0900

    fix: 중복 인증 클래스 제거

--- src/main/java/com/example/gifbottary/user/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.gifbottary.user.repository;
-
-import com.example.gifbottary.user.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    boolean existsByEmail(String email);
-}
