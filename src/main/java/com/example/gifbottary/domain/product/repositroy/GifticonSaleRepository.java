7a7b007f5fa6098e26ea716318e61945c8555beb
easyhyeon3232 <146900921+easyhyeon3232@users.noreply.github.com>
2026-06-25 17:46:46 +0900

    Update GifticonSaleRepository.java

--- src/main/java/com/example/gifbottary/domain/product/repositroy/GifticonSaleRepository.java
+++ src/main/java/com/example/gifbottary/domain/product/repositroy/GifticonSaleRepository.java
@@ -1,4 +1,4 @@
-package com.example.gifbottary.domain.product.repositroy;
+package com.example.gifbottary.domain.product.repository;
 
 import com.example.gifbottary.domain.product.entity.GifticonSale;
 import org.springframework.data.jpa.repository.EntityGraph;
