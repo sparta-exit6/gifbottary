eceb50bc6b72e7c504da3f765569584c3bd31b64
minkim11 <alsgur4772@gmail.com>
2026-06-25 12:48:35 +0900

    refactor: 사용하지 않는 import문 제거

--- src/test/java/com/example/gifbottary/domain/chat/service/ChatRoomServiceTest.java
+++ src/test/java/com/example/gifbottary/domain/chat/service/ChatRoomServiceTest.java
@@ -24,7 +24,6 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
