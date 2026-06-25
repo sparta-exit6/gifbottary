f6d068db4744e00b99686cb2bae8491fe7a6da6b
minkim11 <alsgur4772@gmail.com>
2026-06-25 11:32:08 +0900

    feat: 채팅방 생성
    - 채팅방 생성 시 참여자 chatMember에 등록
    - 이미 존재하는 채팅방일 시 해당 채팅방 반환
    - 추후 리팩토링 시 공통 응답, 예외 발생 처리

--- src/main/java/com/example/gifbottary/domain/chat/repository/ChatRoomRepository.java
+++ src/main/java/com/example/gifbottary/domain/chat/repository/ChatRoomRepository.java
@@ -3,5 +3,8 @@ package com.example.gifbottary.domain.chat.repository;
 import com.example.gifbottary.domain.chat.entity.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+    Optional<ChatRoom> findBySaleIdAndBuyerId(Long saleId, Long buyerId);
 }
