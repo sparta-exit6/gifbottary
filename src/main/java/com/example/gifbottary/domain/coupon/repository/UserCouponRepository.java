ed6a59d5f43f7dfadf34dc2d6b41af40472370ba
minkim11 <alsgur4772@gmail.com>
2026-06-26 20:49:36 +0900

    refactor: 사용하지 않는 import문 삭제

--- src/main/java/com/example/gifbottary/domain/coupon/repository/UserCouponRepository.java
+++ src/main/java/com/example/gifbottary/domain/coupon/repository/UserCouponRepository.java
@@ -2,7 +2,6 @@ package com.example.gifbottary.domain.coupon.repository;
 
 import com.example.gifbottary.domain.coupon.entity.UserCoupon;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
 
 public interface UserCouponRepository extends JpaRepository<UserCoupon, Long> {
     boolean existsByUserIdAndCouponId(Long userId, Long couponId);
