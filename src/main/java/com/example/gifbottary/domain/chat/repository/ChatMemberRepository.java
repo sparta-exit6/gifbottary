b7b61f774e88d7d78c4e2f60d901cb0c506facf6
minkim11 <alsgur4772@gmail.com>
2026-06-26 14:41:46 +0900

    refactor: 필요 없는 코드 삭제

--- src/main/java/com/example/gifbottary/domain/chat/repository/ChatMemberRepository.java
+++ src/main/java/com/example/gifbottary/domain/chat/repository/ChatMemberRepository.java
@@ -3,7 +3,6 @@ package com.example.gifbottary.domain.chat.repository;
 import com.example.gifbottary.domain.chat.entity.ChatMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 
 public interface ChatMemberRepository extends JpaRepository<ChatMember, Long> {
 
