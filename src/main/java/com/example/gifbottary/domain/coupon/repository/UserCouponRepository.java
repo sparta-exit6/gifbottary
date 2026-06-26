c083627161f2f1c51736b8e98138e82a51968257
minkim11 <alsgur4772@gmail.com>
2026-06-26 19:21:06 +0900

    feat: 쿠폰 발급 api 구현
    - 쿠폰 엔티티, 사용자 쿠폰 엔티티 구현 (쿠폰과 사용자 N:M 관계로 중간 테이블(UserCoupon) 구현)
    - 1당 1쿠폰 발급하는 기능
    - 동시성 이슈 검증 테스트 코드 구현

---
+++ src/main/java/com/example/gifbottary/domain/coupon/repository/UserCouponRepository.java
@@ -0,0 +1,10 @@
+package com.example.gifbottary.domain.coupon.repository;
+
+import com.example.gifbottary.domain.coupon.entity.UserCoupon;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface UserCouponRepository extends JpaRepository<UserCoupon, Long> {
+    boolean existsByUserIdAndCouponId(Long userId, Long couponId);
+    List<UserCoupon> findAllByUserId(Long userId);
+}
