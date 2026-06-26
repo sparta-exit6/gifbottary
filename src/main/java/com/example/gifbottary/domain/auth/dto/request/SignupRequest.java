b23726808fd96b7fc8603e2719915d2d2a490b55
Minjae <cmj011112@gmail.com>
2026-06-26 13:05:32 +0900

    refactor : 요구사항 반영

--- src/main/java/com/example/gifbottary/domain/auth/dto/request/SignupRequest.java
+++ src/main/java/com/example/gifbottary/domain/auth/dto/request/SignupRequest.java
@@ -11,15 +11,15 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Getter
 public class SignupRequest {
 
-    @Email
-    @NotBlank
+    @Email(message = "이메일 형식이 올바르지 않습니다.")
+    @NotBlank(message = "이메일은 필수입니다.")
     private String email;
 
-    @NotBlank
-    @Size(min = 8)
+    @NotBlank(message = "비밀번호는 필수입니다.")
+    @Size(min = 8, message = "비밀번호는 8자 이상이어야 합니다.")
     private String password;
 
-    @NotBlank
+    @NotBlank(message = "이름은 필수입니다.")
     private String name;
 
     public User toEntity(PasswordEncoder passwordEncoder) {
