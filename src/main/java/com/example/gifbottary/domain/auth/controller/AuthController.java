094f2df0c465535c9643011689113ab5e487e407
Minjae <cmj011112@gmail.com>
2026-06-29 15:24:06 +0900

    feat: 내 정보 조회 기능 구현

--- src/main/java/com/example/gifbottary/domain/auth/controller/AuthController.java
+++ src/main/java/com/example/gifbottary/domain/auth/controller/AuthController.java
@@ -5,11 +5,14 @@ import com.example.gifbottary.domain.auth.dto.request.LoginRequest;
 import com.example.gifbottary.domain.auth.dto.request.SignupRequest;
 import com.example.gifbottary.domain.auth.dto.response.LoginResponse;
 import com.example.gifbottary.domain.auth.dto.response.LogoutResponse;
+import com.example.gifbottary.domain.auth.dto.response.MyInfoResponse;
 import com.example.gifbottary.domain.auth.dto.response.SignupResponse;
 import com.example.gifbottary.domain.auth.service.AuthService;
+import com.example.gifbottary.domain.user.entity.User;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -37,5 +40,10 @@ public class AuthController {
     @PostMapping("/logout")
     public ApiResponse<LogoutResponse> logout() {
         return ApiResponse.ok(authService.logout());
+    }
+
+    @GetMapping("/me")
+    public ApiResponse<MyInfoResponse> getMyInfo(@AuthenticationPrincipal User user) {
+        return ApiResponse.ok(authService.getMyInfo(user));
     }
 }
